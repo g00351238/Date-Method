9c8fd81cec880c400906e66bd3ec1cce97e58da5
g00351238 <35769279+g00351238@users.noreply.github.com>
2018-01-24 14:42:01 +0000

    version 1

---
+++ js/add-content.js
@@ -0,0 +1,13 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+of (hourNow > 18) {
+	greeting = 'Good evening!';
+} else if (hourNow > 12) {
+	greeting = 'Good afternoon!';
+} else if (hourNow > 0) ;
+	greeting = 'Welcome';
+	}
+	
+document.write('<h3>' + greeting + '</h3');
